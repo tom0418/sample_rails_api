37c34419f1a93365db4bc5051bc6292074995a1f
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-07-12 03:45:10 +0900

    RSpec 導入

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,15 @@ module SampleApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
